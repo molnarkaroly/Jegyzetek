5a8448fb8a3aba6dd742a92d8c2908260ca73274
Molnár Károly <57136811+molnarkaroly210@users.noreply.github.com>
2025-05-28 21:28:27 +0200

    s

---
+++ form_sql_c#/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySqlConnector;
+
+namespace RealEstateGUI
+{
+    public partial class Form1 : Form
+    {
+        static MySqlConnection connection;
+        static List<Seller> activeSellers = new List<Seller>();
+        public Form1()
+        {
+            InitializeComponent();
+            var builder = new MySqlConnectionStringBuilder
+            {
+                Server = "127.0.0.1",
+                UserID = "root",
+                Password = "",
+                Database = "ingatlan"
+            };
+            connection = new MySqlConnection(builder.ConnectionString);
+            connection.Open();
+            // Betöltés
+            buttonActive.BackColor = Color.Red;
+            activeSellers = fullRead();
+            listBoxSellers.Items.Clear();
+            foreach (var item in activeSellers)
+            {
+                listBoxSellers.Items.Add(item.name);
+            }
+        }
+        static List<Seller> activeRead()
+        {
+            List<Seller> a = new List<Seller>();
+            var command = connection.CreateCommand();
+            command.CommandText = "SELECT * FROM sellers WHERE id IN (SELECT sellerid FROM realestates) ORDER BY name;";
+            var reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                Seller tmp = new Seller();
+                tmp.id = reader.GetInt32(0);
+                tmp.name = reader.GetString(1);
+                tmp.phone = reader.GetString(2);
+                a.Add(tmp);
+            }
+            reader.Close();
+            return a;
+        }
+        static List<Seller> fullRead()
+        {
+            List<Seller> a = new List<Seller>();
+            var command = connection.CreateCommand();
+            command.CommandText = "SELECT * FROM sellers ORDER BY name;";
+            var reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                Seller tmp = new Seller();
+                tmp.id = reader.GetInt32(0);
+                tmp.name = reader.GetString(1);
+                tmp.phone = reader.GetString(2);
+                a.Add(tmp);
+            }
+            reader.Close();
+            return a;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            buttonLoad.Enabled = false;
+            listBoxAds.HorizontalScrollbar = true;
+            listBoxAds.ScrollAlwaysVisible = true;
+        }
+
+        private void listBoxSellers_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            labelSellerName.Text = $"Eladó Neve: {activeSellers[listBoxSellers.SelectedIndex].name}";
+            labelSellerPhone.Text = $"Eladó Telefonszáma: {activeSellers[listBoxSellers.SelectedIndex].phone}";
+            labelAdCount.Text = $"Hirdetések Szám: -";
+            listBoxAds.Items.Clear();
+
+            buttonLoad.Enabled = true;
+        }
+
+        private void buttonActive_Click(object sender, EventArgs e)
+        {
+            if (buttonActive.BackColor == Color.Red)
+            {
+                buttonActive.BackColor = Color.Green;
+                activeSellers = activeRead();
+                listBoxSellers.Items.Clear();
+                foreach (var item in activeSellers)
+                {
+                    listBoxSellers.Items.Add(item.name);
+                }
+            }
+            else
+            {
+                buttonActive.BackColor = Color.Red;
+                activeSellers = fullRead();
+                listBoxSellers.Items.Clear();
+                foreach (var item in activeSellers)
+                {
+                    listBoxSellers.Items.Add(item.name);
+                }
+            }
+        }
+
+        private void buttonLoad_Click(object sender, EventArgs e)
+        { 
+            var command = connection.CreateCommand();
+            command.CommandText = "SELECT * FROM realestates WHERE sellerid = " + activeSellers[listBoxSellers.SelectedIndex].id + ";";
+            var reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                string a = "";
+                a += $"Hirdetés Száma: {reader.GetInt64("id")} | ";
+                a += $"Szobák Száma: {reader.GetInt64("rooms")} | ";
+                a += $"Terület: {reader.GetInt64("area")} m^2 | ";
+                a += $"Koordináta: {reader.GetString("latlong")}";
+                listBoxAds.Items.Add(a);
+            }
+            labelAdCount.Text = $"Hirdetések Szám: {listBoxAds.Items.Count}";
+            reader.Close();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            connection.Close();
+        }
+    }
+
+    internal class Category
+    {
+        public int id;
+        public string name;
+    }
+
+    // Seller
+    internal class Seller
+    {
+        public int id;
+        public string name;
+        public string phone;
+    }
+
+    // Ad
+    internal class Ad
+    {
+        public int id;
+        public int rooms;
+        public string latlong;
+        public int floors;
+        public int area;
+        public string description;
+        public bool freeOfCharge;
+        public string imageUrl;
+        public DateTime createAt;
+        public Seller seller;
+        public Category category;
+    }
+}
---
+++ gyak_0528/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApp1
+{
+
+    internal class eloado
+    {
+        public string azonosító;
+        public string név;
+        public string születési_év;
+        public string származás;
+        public string feliratkozó_szám;
+    }
+
+
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        List<eloado> eloadok = new List<eloado>();
+
+        void beolvas()
+        {
+            string[] sorok = File.ReadAllLines("data.csv");
+            foreach (string sor in sorok)
+            {
+                string[] adatok = sor.Split(',');
+                eloado ee = new eloado();
+                ee.azonosító = adatok[0];
+                if (ee.azonosító == "azonosító") continue;
+                ee.név = adatok[1];
+                ee.születési_év = adatok[2];
+                ee.származás = adatok[3];
+                ee.feliratkozó_szám = adatok[4];
+                eloadok.Add(ee);
+            }
+        }
+
+        ListBox listBox1 = new ListBox();
+        TextBox textBox1 = new TextBox();
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = "Előadók listája";
+            beolvas();
+            Button button1 = new Button();
+            button1.Location = new Point(10, 10);
+            button1.Text = "Old";
+            button1.Click += Button1_Click;
+            this.Controls.Add(button1);
+
+            listBox1.Height = 400;
+            listBox1.Location = new Point(10, 50);
+            listBox1.SelectedIndexChanged += ListBox1_SelectedIndexChanged;
+
+            foreach (eloado ee in eloadok)
+            {
+                listBox1.Items.Add(ee.név);
+            }
+
+            this.Controls.Add(listBox1);
+
+            textBox1.Location = new Point(250, 20);
+            textBox1.Width = 200;
+            textBox1.Height = 400;
+            textBox1.Multiline = true;
+            textBox1.ReadOnly = true;
+            this.Controls.Add(textBox1);
+        }
+
+        bool felsorolva = false;
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear(); // Mindig ürítsd a listát a frissítés előtt
+
+            if (felsorolva == true) // Jelenleg az összes elem van felsorolva, most szűrjünk
+            {
+                foreach (eloado ee in eloadok)
+                {
+                    if (int.TryParse(ee.születési_év, out int birthYear))
+                    {
+                        if (birthYear > 2000)
+                        {
+                            listBox1.Items.Add(ee.név); // Add a név-et, nem a születési évet, ha a listán az előadók nevei szerepelnek
+                        }
+                    }
+                }
+                felsorolva = false; // Átállítjuk a flag-et, mert most a szűrt lista van kijelezve
+            }
+            else // Jelenleg a szűrt lista van felsorolva, most az összeset mutassuk
+            {
+                foreach (eloado ee in eloadok)
+                {
+                    listBox1.Items.Add(ee.név); // Add az összes nevet
+                }
+                felsorolva = true; // Átállítjuk a flag-et, mert most az összes elem van kijelezve
+            }
+
+        }
+
+        private void ListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            if (listBox1.SelectedIndex >= 0 && listBox1.SelectedIndex < eloadok.Count)
+            {
+                eloado selectedEloado = eloadok[listBox1.SelectedIndex];
+                textBox1.Text += $"Azonosító: {selectedEloado.azonosító}\n";
+                textBox1.Text += $"Név: {selectedEloado.név}\n";
+                textBox1.Text += $"Születési év: {selectedEloado.születési_év}\n";
+                textBox1.Text += $"Származás: {selectedEloado.származás}\n";
+                textBox1.Text += $"Feliratkozó szám: {selectedEloado.feliratkozó_szám}\n";
+
+            }
+
+        }
+    }
+}
